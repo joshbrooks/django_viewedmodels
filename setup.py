bbd01dc48a242d8a86ba8a20be679e6d8d30d306
Joshua Brooks <josh.vdbroek@gmail.com>
2018-03-07 17:20:01 +0900

    New release with support for "vacuum"

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_viewedmodels',
-    version='0.1.22',
+    version='0.1.24',
     packages=['viewedmodels', 'viewedmodels.management.commands'],
     include_package_data=True,
     license='GPLv3',  # example license
     description='A Django app to define Postgres Views as Models',
     long_description=read('README.rst'),
     url='https://github.com/joshbrooks/django_viewedmodels/',
-    download_url='https://github.com/joshbrooks/django_viewedmodels/archive/0.1.22.tar.gz',
+    download_url='https://github.com/joshbrooks/django_viewedmodels/archive/0.1.24.tar.gz',
     author='Joshua Brooks',
     author_email='josh@catalpa.io',
     install_requires=[
